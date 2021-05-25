ff64ff858dacc38b458c4210c95ba6f1d6df3189
geektuhin123 <geektuhin@gmail.com>
2021-05-25 23:41:03 +0530

    feat: add API to update dateTime

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { exec } = require("child_process");
+
+// sudo timedatectl set-ntp false
+// sudo date -s "${dateFormat('5/25/2021 20:48:22', 'UTC:mm/dd/yyyy HH:MM:ss')}" --utc
+// sudo timedatectl set-timezone "Asia/Kolkata"
+// sudo hwclock -w --utc
+
+const setDateTime = (dateTime, options) => {
+  let sudo = options.useSudo ? "sudo " : "";
+
+  // To disable NTP time synchronization
+  exec(`${sudo}timedatectl set-ntp false`);
+  // set date time
+  // exec(`${sudo}date -s "${dateFormat(dateTime, 'UTC:mm/dd/yyyy HH:MM:ss')}" --utc`);
+  exec(`${sudo}timedatectl set-time '${dateTime}'`);
+
+  // set timezone
+  if (options.timeZone) {
+    exec(`${sudo}timedatectl set-timezone "${options.timeZone}"`);
+  }
+  //Sync the hardware clock.”
+  exec(`${sudo}hwclock -w --utc`);
+};
+
+module.exports = setDateTime;
